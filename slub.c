8b5786b6e40102de5978436c1b0948bf78114e04
Allen Pais <allen.lkml@gmail.com>
2020-05-21 15:57:18 +0530

    lkm: basic slub test kernel module

---
+++ slub.c
@@ -0,0 +1,42 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/mm.h>
+
+static struct kmem_cache *slub;
+
+struct A {
+	int a;
+	int b;
+};
+
+int slub_create_kmem(void)
+{
+	int ret = -1;
+	slub = kmem_cache_create("slub-alloc", sizeof(struct A), 0, 0, NULL);
+	if (slub != NULL) {
+		printk("Successfully created kem for slub\n");
+		ret = 0;
+	}
+
+	return ret;
+}
+
+static int __init slub_mod_init(void)
+{
+	int ret;
+	printk("Slub lkm\n");
+	ret = slub_create_kmem();
+	return 0;
+}
+
+static void __exit slub_mod_exit(void)
+{
+	printk("Slub lkm exit\n");
+	kmem_cache_destroy(slub);
+}
+
+module_init(slub_mod_init);
+module_exit(slub_mod_exit);
+
+MODULE_LICENSE("GPL");
